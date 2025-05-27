e9bd0cce7a17b2c93883af2516f2fc1718b71e84
bonasieric <eric.berntsson@elev.ga.lbs.se>
2025-05-27 16:19:33 +0200

    Canavs grejer

--- Assets/Script/SoundManager.cs
+++ Assets/Script/SoundManager.cs
@@ -19,7 +19,7 @@ public class SoundManager : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.V))
         {
             PlaySound(gunShot);
         }
