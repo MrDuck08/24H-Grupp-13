32dfa8b1c5eaff434420ba942879d881b25e1262
Isak Logenius <isak.logenius@gmail.com>
2025-05-27 09:39:21 +0200

    Revert "Bar Acceleration"
    This reverts commit d4cc5a7e2d5e193423d66eeb88229752d39bfb72.

--- Assets/Script/PatienceBar.cs
+++ Assets/Script/PatienceBar.cs
@@ -8,20 +8,14 @@ public class PatienceBar : MonoBehaviour
     Image bar;
 
     [SerializeField]
-    float startSpeed;
-
-    [SerializeField]
-    float acceleration;
+    float speed;
 
     float patience;
-    float speed;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         patience = 1.0f;
-
-        speed = startSpeed;
     }
 
     // Update is called once per frame
@@ -30,7 +24,6 @@ public class PatienceBar : MonoBehaviour
         // Patience
         patience -= speed * Time.deltaTime;
         patience = math.clamp(patience, 0.0f, 1.0f);
-        speed += acceleration * Time.deltaTime;
 
         // UI
         bar.transform.localScale = new Vector3(patience, 1, 1);
